8dc0a2ea974bf4a46f208fa48ccea3eeca4ae26e
alvaro <alvarosimeao21@gmail.com>
2025-10-18 14:43:15 -0300

    subindo código inicial

--- tela2.js
+++ tela2.js
@@ -1,34 +1,37 @@
-
-
 // Pega oque vem depois da ?
 const id_url = new URLSearchParams (window.location.search);
 
-const id = id_url.get("id");
+// Pega so o valr recebido da id_url
+const vlr_id = id_url.get("id");
 
 
 // Busca o personagem pelo ID
-async function getCharacter(id) {
-  const response = await fetch(`https://rickandmortyapi.com/api/character/${id}`);
+async function getCharacter(vlr_id) {
+  const response = await fetch(`https://rickandmortyapi.com/api/character/${vlr_id}`);
   const data = await response.json();
   return data;
 }
 
 async function showCharacter() {
-  const character = await getCharacter(id);
+  const character = await getCharacter(vlr_id);
 
   const container = document.getElementById("container");
 
   const box = document.createElement("div");
-  box.classList.add("char-box");
+  box.classList.add("box");
 
   const gender = document.createElement("h2")
-  gender.textContent = character.gender;
+  gender.textContent ="Genero: " + character.gender;
+
+  const origin = document.createElement("h2")
+  origin.textContent = "Origem: " + character.origin.name
 
   const img = document.createElement("img");
+  img.classList.add("img")
   img.src = character.image;
 
   const name = document.createElement("h1");
-  name.textContent = character.name;
+  name.textContent = "Nome: " + character.name;
 
   const specie = document.createElement("h2");
   specie.textContent = "Espécie: " + character.species;
@@ -36,12 +39,31 @@ async function showCharacter() {
   const status = document.createElement("h2");
   status.textContent = "Status: " + character.status;
 
+  const type = document.createElement("h2");
+  type.textContent = "Tipo: " + character.type
+  
+  const location = document.createElement("h2");
+  location.textContent = "Localizaçao: " + character.location.name
+
+  // const episode = document.createElement("h2")
+  // episode.textContent = "Episodio(s): " + character.episode
+
   box.appendChild(img);
   box.appendChild(name);
-  box.appendChild(specie);
+  box.appendChild(gender)
   box.appendChild(status);
+  box.appendChild(specie);
+  box.appendChild(origin)
+  box.appendChild(location)
+  box.appendChild(type)
+  // box.appendChild(episode)
 
   container.appendChild(box);
 }
 
 showCharacter();
+
+back.addEventListener("click", function()  {
+        window.location.href = "tela1.html"
+
+    })
